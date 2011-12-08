243b951608b1a53f76811ebb141c25dd98b03f63
David Salazar <Eagle114th@gmail.com>
2011-12-08 15:32:02 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DXSFinalProject::Application.config.secret_token = '2a83405e0bc2da3751481fe17af3b1eb2f2e9f095cfacea1605da7a0aaa21d2c2b749a231cbaccdd3e5307a901b61b252671fcf8a3f071eb6f7a11f37f2efdff'
