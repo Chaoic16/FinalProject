20357c09e299e2c198e880cd81ee1824fb740ec3
David Salazar <Eagle114th@gmail.com>
2011-12-08 15:42:36 -0600

    Created branch model

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
+require 'spork'
+
+Spork.prefork do
+
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
@@ -30,4 +35,6 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+end
+
 end
