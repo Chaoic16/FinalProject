4d77b3c987faac058dc863795f2a5dab4b3da518
David Salazar <Eagle114th@gmail.com>
2011-12-08 18:14:00 -0600

    restored my project page and changed all pages colors

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,45 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def contact
+    @title = "Contact"
   end
-
+  
+  def about
+    @title = "About"
+  end
+  
+  def game
+    @title = "Game List"
+  end
+  
+  def developer
+    @title = "Developer Team List"
+  end
+  
+  def publisher
+    @title = "Publisher List"
+  end
+  
+  def platform
+    @title = "Platform List"
+  end
+  
+  def feedback
+    @title = "Feedback"
+  end
+  
+  def registry
+    @title = "Registration"
+  end
+  
+  def forgot
+    @title = "Forgot password"
+  end
+  
+  def help
+    @title = "Help"
+  end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
+
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => "Home")
     end
   end
 
@@ -14,6 +21,118 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => "Contact")
+    end
   end
 
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", :content => "About")
+    end
+  end
+  
+  describe "GET 'game'" do
+    it "returns http success" do
+      get 'game'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'game'
+      response.should have_selector("title", :content => "Game List")
+    end
+  end
+  
+  describe "GET 'develpoer'" do
+    it "returns http success" do
+      get 'developer'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'developer'
+      response.should have_selector("title", :content => "Developer Team List")
+    end
+  end
+  
+  describe "GET 'publisher'" do
+    it "returns http success" do
+      get 'publisher'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'publisher'
+      response.should have_selector("title", :content => "Publisher List")
+    end
+  end
+  
+  describe "GET 'platform'" do
+    it "returns http success" do
+      get 'platform'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'platform'
+      response.should have_selector("title", :content => "Platform List")
+    end
+  end
+  
+  describe "GET 'feedback'" do
+    it "returns http success" do
+      get 'feedback'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'feedback'
+      response.should have_selector("title", :content => "Feedback")
+    end
+  end
+  
+  describe "GET 'registry'" do
+    it "returns http success" do
+      get 'registry'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'registry'
+      response.should have_selector("title", :content => "Registration")
+    end
+  end
+  
+  describe "GET 'forgot'" do
+    it "returns http success" do
+      get 'forgot'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'forgot'
+      response.should have_selector("title", :content => "Forgot password")
+    end
+  end
+  
+  describe "GET 'help'" do
+    it "returns http success" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", :content => "Help")
+    end
+  end
 end
